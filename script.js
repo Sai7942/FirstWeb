eeb62233378a64fea43b2634e5e1e1455b10a721
Sai7942 <dharahas04@gmail.com>
2024-12-28 09:02:38 +0530

    latest commit

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ window.onload = function() {
 function logEmail() {
     console.log("Email: abc@gmail.com");
 }
-document.addEventListener("DOMContentLoaded", function() {
-    const emailParagraph = document.querySelector("main p:nth-of-type(2)");
-    emailParagraph.addEventListener("click", logEmail);
-});
